7e9c65d250e30568d6bce6df2b237a86be9aa391
phongpisut <phongpisut@users.noreply.github.com>
2024-03-24 08:28:39 +0000

    deploy: 5d731a2e0184271a8da47ac00213e8f226f4e173

---
+++ assets/index-DgYOZ5r4.js
@@ -0,0 +1,7 @@
+import{_ as i}from"./index-bWti3d9w.js";async function p(t,a=!0){await t.addParticleUpdater("opacity",async r=>{const{OpacityUpdater:e}=await i(()=>import("./OpacityUpdater-DIpzcPb_.js"),__vite__mapDeps([0,1,2]));return new e(r)},a)}export{p as loadOpacityUpdater};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/OpacityUpdater-DIpzcPb_.js","assets/index-bWti3d9w.js","assets/index-BBh8weXX.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
