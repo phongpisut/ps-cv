528bf047c79718588844f6e552a7ad8ace8f5177
phongpisut <phongpisut@users.noreply.github.com>
2024-03-25 16:22:08 +0000

    deploy: db65e7fcfd77cd9260bb302b1297e035bf49c734

--- assets/index-BkeNpEh2.js
+++
@@ -1,7 +0,0 @@
-import{_ as i}from"./index-BHb-qfex.js";async function o(a,e=!0){const{CircleDrawer:r}=await i(()=>import("./CircleDrawer-CGM2Sn-1.js"),__vite__mapDeps([0,1,2]));await a.addShape("circle",new r,e)}export{o as loadCircleShape};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/CircleDrawer-CGM2Sn-1.js","assets/index-BHb-qfex.js","assets/index-B1ZMKTyL.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
