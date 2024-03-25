a239d7ff5c92a673ddb18fe90c3631b0766a4c4d
phongpisut <phongpisut@users.noreply.github.com>
2024-03-25 16:30:59 +0000

    deploy: f6160babfca3fcfc64c5e5a5017ea32b76b6b25d

---
+++ assets/index-CCgj9QbA.js
@@ -0,0 +1,7 @@
+import{_ as o}from"./index-DR5XSWv9.js";async function i(t,a=!0){await t.addParticleUpdater("outModes",async e=>{const{OutOfCanvasUpdater:r}=await o(()=>import("./OutOfCanvasUpdater-DGCMSmHs.js"),__vite__mapDeps([0,1,2]));return new r(e)},a)}export{i as loadOutModesUpdater};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/OutOfCanvasUpdater-DGCMSmHs.js","assets/index-DR5XSWv9.js","assets/index-B1ZMKTyL.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
